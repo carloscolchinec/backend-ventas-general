4de4c0746ed955d3833187c88a076949169070d1
seroficomsa <soporte@seroficom.org>
2026-01-15 19:59:24 -0500

    test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = Number(process.env.PORT) || 3100;
+const PORT = Number(process.env.PORT) || 3150;
 
 const startServer = async () => {
     await connectDB();
