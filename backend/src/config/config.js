bed95586c28ccd2b471155f382121394edccdf5a
seroficomsa <soporte@seroficom.org>
2026-01-15 19:54:13 -0500

    test

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,23 +3,16 @@ require('dotenv').config();
 module.exports = {
     development: {
         username: process.env.DB_USERNAME,
-        password: process.env.DB_password, // Note: lowercase 'password' matches your .env usage
+        password: process.env.DB_PASSWORD, // <--- Cambiado a MAYÚSCULAS
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: 'mysql',
         logging: console.log
     },
-    test: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_password,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: 'mysql'
-    },
     production: {
         username: process.env.DB_USERNAME,
-        password: process.env.DB_password,
+        password: process.env.DB_PASSWORD, // <--- Cambiado a MAYÚSCULAS
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: 'mysql'
